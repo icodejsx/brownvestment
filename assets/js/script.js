87fb2fc99756b6243142609abccd230a4d8bac83
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2022-12-16 12:17:10 +0100

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,36 @@
 	"use strict";
 
 
+
+	let question = document.querySelectorAll(".question");
+
+	question.forEach(question => {
+	  question.addEventListener("click", event => {
+		const active = document.querySelector(".question.active");
+		if(active && active !== question ) {
+		  active.classList.toggle("active");
+		  active.nextElementSibling.style.maxHeight = 0;
+		}
+		question.classList.toggle("active");
+		const answer = question.nextElementSibling;
+		if(question.classList.contains("active")){
+		  answer.style.maxHeight = answer.scrollHeight + "px";
+		} else {
+		  answer.style.maxHeight = 0;
+		}
+	  })
+	})
+	
+
+
+
+
+
+
+
+
+
+
 	//Hide Loading Box (Preloader)
 	function handlePreloader() {
 		if($('.loader-wrap').length){
